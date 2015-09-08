17e0d330013e53b6bf5e714863c6a64f65582277
Kees Jongenburger <kees.jongenburger@gmail.com>
2015-09-08 09:10:00 +0200

    mkvendor.sh: Initial commit of FP2

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := FP2
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/fairphone/FP2/device_FP2.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := FP2
+PRODUCT_NAME := cm_FP2
+PRODUCT_BRAND := fairphone
+PRODUCT_MODEL := FP2
+PRODUCT_MANUFACTURER := fairphone
